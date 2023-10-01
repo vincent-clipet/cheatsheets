8b5859e2e992d4134b37de60bbabe1144cf212fb
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-10-02 01:35:50 +0200

    feat: created a 'new' CSS theme for all MD -> HTML conversions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,14 @@
 $(function () {
-	$("section h1, section h2, section h3").each(function () {
-		$("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '') + "'>" + $(this).text() + "</a></li>");
+	$("#wrapper h2, #wrapper h3").each(function () {
+		$("#table_of_content ol").append(
+			"<li class='tag-"
+			+ this.nodeName.toLowerCase()
+			+ "'><a href='#"
+			+ $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '')
+			+ "'>"
+			+ $(this).text()
+			+ "</a></li>");
 		$(this).attr("id", $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, ''));
-		$("nav ul li:first-child a").parent().addClass("active");
-	});
-
-	$("nav ul li").on("click", "a", function (event) {
-		var position = $($(this).attr("href")).offset().top - 190;
-		$("html, body").animate({ scrollTop: position }, 400);
-		$("nav ul li a").parent().removeClass("active");
-		$(this).parent().addClass("active");
-		event.preventDefault();
+		$("#table_of_content ul li:first-child a").parent().addClass("active");
 	});
 });
