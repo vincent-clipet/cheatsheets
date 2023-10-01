fbd2e73ec93ba0731d21544979437f4ab3c24a65
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-10-02 01:49:41 +0200

    fix: don't show the table of content if there are no <h2> or <h3> at the top level of tha page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,21 @@
 $(function () {
-	$("#wrapper h2, #wrapper h3").each(function () {
-		$("#table_of_content ol").append(
-			"<li class='tag-"
-			+ this.nodeName.toLowerCase()
-			+ "'><a href='#"
-			+ $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '')
-			+ "'>"
-			+ $(this).text()
-			+ "</a></li>");
-		$(this).attr("id", $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, ''));
-		$("#table_of_content ul li:first-child a").parent().addClass("active");
-	});
+	if ($("#wrapper > h2, #wrapper > h3").length > 0) {
+		$("#wrapper > h2, #wrapper > h3").each(function () {
+			$("#table_of_content ol").append(
+				"<li class='tag-"
+				+ this.nodeName.toLowerCase()
+				+ "'><a href='#"
+				+ $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '')
+				+ "'>"
+				+ $(this).text()
+				+ "</a></li>");
+			$(this).attr("id", $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, ''));
+			$("#table_of_content ul li:first-child a").parent().addClass("active");
+		});
+	}
+	else {
+		$("#table_of_content").remove();
+	}
+
+
 });
